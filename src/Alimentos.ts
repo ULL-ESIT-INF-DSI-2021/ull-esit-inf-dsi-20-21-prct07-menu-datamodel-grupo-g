db766ef4a621c3a2407006602ec6d131898491c8
Aitor Alonso <alu0101122496@ull.edu.es>
2021-04-07 18:34:45 +0100

    Prueba

---
+++ src/Alimentos.ts
@@ -0,0 +1,5 @@
+abstract class Alimentos {
+  constructor(private nombre: string, private origen: string, private hidratos: number, 
+    private proteinas: number, private lipidos: number, private precio: number){
+  }
+}
