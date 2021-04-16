3a8c2ce8d196656d26acd2e9760e6692f6467dd7
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-04-16 18:26:57 +0100

    practica 8 0.3

--- src/Food.ts
+++ src/Food.ts
@@ -1,4 +1,4 @@
-type FoodGroup = "Carne" | "Pescado" |"Huevos" | "Tofu" | "Frutos secos" | "Semillas" | "Legumbres" | "Verduras" | "Hortalizas" | "Leche" | "Quesos" | "Cereales" | "Frutas"; 
+type FoodGroup = "Carne" | "Pescado" | "Moluscos" |"Huevos" | "Tofu" | "Frutos secos" | "Semillas" | "Legumbres" | "Verduras" | "Hortalizas" | "Leche" | "Quesos" | "Embutidos" | "Cereales" | "Frutas"; 
 
 export class Food {
   constructor(private name: string, private group: FoodGroup, private origin: string, private hydrates: number, 
