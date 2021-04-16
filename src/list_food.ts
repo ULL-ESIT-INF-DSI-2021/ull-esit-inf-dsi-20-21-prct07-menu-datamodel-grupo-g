780600165e0879239dbd7045e33c41ff9563ae6e
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-04-16 15:40:02 +0100

    practica 8 0.2

---
+++ src/list_food.ts
@@ -0,0 +1,69 @@
+import {Food} from './Food';
+
+/*Nombre, tipo, origen, hidratos, proteinas, lipidos, precio*/
+/*un kilo de huevos de gallina son 12 huevos*/
+/*un kilo de huevos de codorniz son 100 huevos*/
+
+export let arroz_blanco = new Food("Arroz blanco", "Cereales", "Madrid", 86, 7, 0.9, 1.5);
+export let arroz_integral = new Food("Arroz integral", "Cereales", "Madrid", 73.4, 8, 2.2, 1.08);
+
+export let leche_desnatada = new Food("Leche desnatada", "Leche", "Madrid", 4.6, 3.6, 0.3, 0.79);
+export let leche_semidesnatada = new Food("Leche semidesnatada", "Leche", "Madrid", 4.6, 3.3, 1.6, 0.84);
+export let leche_entera = new Food("Leche entera", "Leche", "Madrid", 4.7, 3.1, 3.5, 0.79);
+
+export let queso_azul = new Food("Queso azul", "Quesos", "Francia", 0.1, 21.1, 29.8, 13.50);
+export let queso_rallado = new Food("Queso rallado", "Quesos", "Madrid", 0.5, 40, 28.9, 8.95);
+
+export let huevo_gallina = new Food("Huevo de gallina", "Huevos", "Madrid", 0.7, 12.5, 11.1, 1.99);
+export let huevo_codorniz = new Food("Huevo de codorniz", "Huevos", "Madrid", 0.41, 13.1, 11.2, 7.41);
+
+export let chuleta_cordero = new Food("Chuleta de cordero", "Carne", "Madrid", 0, 18, 17, 17.95);
+export let chuleta_ternera = new Food("Chuleta de ternera", "Carne", "Madrid", 0, 17, 20.5, 17.40);
+export let solomillo_ternera = new Food("Solomillo de ternera", "Carne", "Madrid", 0, 18.2, 3.5, 36);
+export let muslo_pavo = new Food("muslo de pavo", "Carne", "Madrid", 0, 18.9, 8.4, 3.5);
+export let pechuga_pollo = new Food("pechuga de pollo", "Carne", "Madrid", 0, 21.8, 2.8, 7.55);
+export let muslo_pollo = new Food("muslo de pollo", "Carne", "Madrid", 0, 17.9, 4.4, 3.95);
+/*
+Verduras revisar
+export let ajo
+export let champiñón
+export let espinacas
+export let lechuga
+export let repollo
+export let rúcula
+
+Hortalizas
+export let zanahorias
+export let calabacín
+export let berenjena
+export let cebolla
+export let tómate
+export let patata
+
+Frutas
+export let aguacate
+export let caqui
+export let cereza
+export let ciruela
+export let frambuesa
+export let fresa
+export let kiwi
+export let lima
+export let limón
+export let mandarina
+export let naranja
+export let mango
+export let manzana
+export let melocotón
+export let melón
+export let pera
+export let piña
+export let plátano
+export let sandía
+export let uvas
+*/
+
+
+
+
+
