b06383a8d50a88524b676533097f4be4fb249445
Carla Olivares <alu0101120218@ull.edu.es>
2021-04-17 19:32:16 +0100

    clase carta / MenuCard

--- src/list_menu.ts
+++ src/list_menu.ts
@@ -1,7 +1,7 @@
-import {Food} from './Food'
+import { Food } from './Food'
+//import { Menu } from '/.Menu'
 import * as food from './list_food'
 import * as plates from './list_plates'
 import { Plate } from './Platos'
-import { Menu } from '/.Menu'
 
 //let menu1 = new Menu(entrante, primerPlato, segundoPlato, postre);
