7f2639a06ad38ee273f0bdb9300c58e0c83849ad
Aitor Alonso <alu0101122496@ull.edu.es>
2021-04-17 19:43:13 +0100

    Comanda 0.5

--- src/list_menu.ts
+++ src/list_menu.ts
@@ -2,6 +2,6 @@ import {Food} from './Food'
 import * as food from './list_food'
 import * as plates from './list_plates'
 import { Plate } from './Platos'
-import { Menu } from '/.Menu'
+import { Menu } from './Menu'
 
 //let menu1 = new Menu(entrante, primerPlato, segundoPlato, postre);
